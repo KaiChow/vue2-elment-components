a29730c006d60790db3287dd9be06b7b4c817556
KevinZhou <1065438834@qq.com>
2023-05-22 14:27:13 +0800

    基础目录

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,14 @@
-const { defineConfig } = require('@vue/cli-service')
+const { defineConfig } = require("@vue/cli-service");
+const path = require("path");
 module.exports = defineConfig({
-  transpileDependencies: true
-})
+  transpileDependencies: true,
+  configureWebpack: {
+    resolve: {
+      extensions: [".js", ".vue", ".json"],
+      alias: {
+        vue$: "vue/dist/vue.esm.js",
+        "@": path.join(__dirname, "/", "src"),
+      },
+    },
+  },
+});
